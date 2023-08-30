238a3171337341eb6e267deb00077048c330242d
Cee1414 <kai971493@gmail.com>
2023-08-30 00:01:58 -0500

    now able to read user input

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import discord
 import os
+import responses
 
+async def send_message(message, user_message, is_private):
+        try:
+                response = response.handle_response(user_message)
+                await message.author.send(response) if is_private else await message.channel.send(response)
+        except Exception as e:
+                print(e)
 
 
 def run_discord_bot(): 
@@ -20,6 +27,19 @@ def run_discord_bot():
         @client.event
         async def on_ready():
                 print(f"{client.user} is now running")
-        
+
+        @client.event
+        async def on_message(message):
+                if message.author == client.user:
+                        return
+                        
+                username = str(message.author)
+                user_message = str(message.content)
+                channel = str(message.channel)
+
+                print(f"{username} said: '{user_message}' ({channel})")
+
+
+
         client.run(TOKEN)
     
---
+++ responses.py
@@ -0,0 +1,9 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == "hello":
+        return "hey there!"
+    
+
+    
+    return "IDK what u said"
